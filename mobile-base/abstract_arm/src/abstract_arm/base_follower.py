fixed rotation bug, but introduced small amounts of drift

mobile-base/abstract_arm/src/abstract_arm/base_follower.py
@@ -5,6 +5,7 @@ from std_msgs.msg import Float64
 from tf2_msgs.msg import TFMessage
 from tf import TransformListener
 from tf.transformations import euler_from_quaternion
+import numpy as np
 
 
 class Follower(object):
@@ -14,6 +15,10 @@ class Follower(object):
 
         self.arm_controllers = []
         self.prev_value = [0,0,0]
+        self.z_rot_diff = 0
+        self.z_rot_prev = 0
+        self.z_rot_global = 0 
+        self.rot_counter = 0
 
         arm_name = rospy.get_param("arm/name")
         print("arm: " + arm_name)
@@ -31,15 +36,91 @@ class Follower(object):
 
         rospy.spin()
 
+    
+    def quadrantCheck(self, angle):
+        """
+        This is a helper function designed to quickly evaluate which quadrant an angle in -pi,pi is in
+        """
+        if angle > 0:
+            if angle > np.pi/2:
+                return 2
+            else:
+                return 1
+        else:
+            if angle < -np.pi/2:
+                return 3
+            else:
+                return 4
+    
     def callback(self, data):
         try:
             # get position and quaternion between world and base
             position, quaternion = self.tf.lookupTransform("world", self.root_joint_name, rospy.Time())
-
+            
             if position != [0.0,0.0,0.0]:    
                 # print(position)
 
                 euler = euler_from_quaternion(quaternion) # translate to euler
+                euler = np.asarray(euler)
+                
+                quadrantCurrent = self.quadrantCheck(euler[2])
+                quadrantPrev    = self.quadrantCheck(self.z_rot_prev)
+                edgeCase = False
+
+                # Check if we cross the -pi,pi x axis line or the 0 line! We handle this as an edge case
+                if quadrantCurrent!= quadrantPrev:
+                    if quadrantCurrent == 3 and quadrantPrev == 2:
+                        # We've crossed the -pi,pi border going CCW
+                        self.z_rot_diff = abs(euler[2]) + self.z_rot_prev - 2*np.pi
+                        edgeCase = True
+
+                    elif quadrantCurrent == 2 and quadrantPrev == 3:
+                        # We've crossed the -pi,pi border going CW
+                        self.z_rot_diff = -(euler[2] + abs(self.z_rot_prev) - 2*np.pi)
+                        edgeCase = True
+                    
+                    elif quadrantCurrent == 1 and quadrantPrev == 4:
+                        # We've crossed the 0 border going CCW
+                        self.z_rot_diff = euler[2] + abs(self.z_rot_prev) 
+                        edgeCase = True
+                        
+
+                    elif quadrantCurrent == 4 and quadrantPrev == 1:
+                        self.z_rot_diff = -(abs(euler[2]) + self.z_rot_prev)
+                        edgeCase = True
+                        
+                
+
+                ## Normal operation
+               
+                if quadrantCurrent in [1,2] and quadrantPrev in [1,2]: # both in Quadrants I or II
+                    # Determine direction
+                    if euler[2] > self.z_rot_prev: # moving CCW
+                        self.z_rot_diff = euler[2] - self.z_rot_prev
+                    else: ## moving CW
+                        self.z_rot_diff = -(self.z_rot_prev - euler[2])
+                elif quadrantCurrent in [3,4] and quadrantPrev in [3,4]:
+                    # Determine direction
+                    if euler[2] > self.z_rot_prev: # moving CCW
+                        self.z_rot_diff = abs(self.z_rot_prev) - abs(euler[2])
+                    else: # moving CW
+                        self.z_rot_diff = -(abs(euler[2])- abs(self.z_rot_prev))
+                
+                else:
+                    print("Weird edge case ran")
+                    print(f"Quadrants: {quadrantCurrent, quadrantPrev}")
+                    print(f"Angles: {euler[2], self.z_rot_prev}")
+
+               
+            
+                self.rot_counter += self.z_rot_diff
+                print(f"rot_counter {self.rot_counter}")
+            
+
+                # Send positions!
+                
+               
+
                 curr_val = (position[0], position[1], euler[2]) # x, y pos and z rotation
                 # print(curr_val)
 
@@ -51,10 +132,11 @@ class Follower(object):
                 # if all(d < .2 for d in differences):
                     # publish to arm
 
-                self.arm_controllers[0].publish(curr_val[0])
-                self.arm_controllers[1].publish(curr_val[1])
-                self.arm_controllers[3].publish(curr_val[2])
-
+                print(f"euler: {euler}")
+                self.arm_controllers[0].publish(curr_val[0]) # x 
+                self.arm_controllers[1].publish(curr_val[1]) # y
+                self.arm_controllers[3].publish(self.rot_counter) # z_rot
+                self.z_rot_prev = euler[2]
                 # self.arm_controllers[2].publish(1)
                 
                 # self.prev_value = curr_val

mobile-base/mobile_base_control/src/quad_prog_test.py
@@ -11,10 +11,14 @@ from tf import TransformListener
 from scipy.spatial.transform import Rotation as R
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from std_msgs.msg import Float64MultiArray, MultiArrayLayout
+from std_msgs.msg import Float64
+from tf.transformations import euler_from_quaternion
+
+from visualization_msgs.msg import Marker
 
 class holistic_control():
 
-    def __init__(self):
+    def __init__(self, goalT):
         # Initialize Node
         rospy.init_node("holistic_control")
         self.arm = rospy.get_param("/arm/name")
@@ -27,7 +31,10 @@ class holistic_control():
         arm_trajectory_txt = "{arm}/gazebo_ros_control/command"
         self.panda_base_arm = panda()
         print(f"self.panda_base_arm: {self.panda_base_arm}")
-        
+        self.R = 0.1524 # wheel radius [m]
+        self.W = 0.56848 # distance between the two wheels [m]
+        self.lx = self.W/2 # represent the distance from the robot's center to the wheels projected on the x and y axis
+        self.ly = 0.5436278 # represent the distance from the robot's center to the wheels projected on the x and y axis
 
 
         self.panda_arm = rtb.models.ETS.Panda() # instantiate arm from rtb
@@ -38,7 +45,10 @@ class holistic_control():
         rospy.Subscriber("/base/joint_states", JointState, self.baseCallback, queue_size=10)
 
         # Hookup Base Velocities
-
+        base_controller_str = "/base/base_{0}_joint_controller/command"
+        self.Vx_world = rospy.Publisher(base_controller_str.format('x'), Float64, queue_size=10)
+        self.Vy_world = rospy.Publisher(base_controller_str.format('y'), Float64, queue_size=10)
+        self.W_z_world = rospy.Publisher("/base/base_z_rotation_controller/command", Float64, queue_size=10)
 
         # Publishers
         # self.traj_client = rospy.Publisher(arm_trajectory_txt.format(arm=self.arm), JointTrajectory, queue_size=10)
@@ -47,15 +57,47 @@ class holistic_control():
 
 
         self.arm_joint_names = [f"{self.arm}_joint1",
-                            f"{self.arm}_joint2",
-                            f"{self.arm}_joint3",
-                            f"{self.arm}_joint4",
-                            f"{self.arm}_joint5",
-                            f"{self.arm}_joint6",
-                            f"{self.arm}_joint7"]
+                                f"{self.arm}_joint2",
+                                f"{self.arm}_joint3",
+                                f"{self.arm}_joint4",
+                                f"{self.arm}_joint5",
+                                f"{self.arm}_joint6",
+                                f"{self.arm}_joint7"]
+
+        # Initialize Marker in RVIZ
+        mark = Marker()
+        mark.header.frame_id = "world"
+        mark.header.stamp = rospy.Time.now()
+        mark.ns = "panda"
+        mark.id = 0 #arrow
+        mark.type = 0
+        # mark.action = Marker().ADD
+        mark.pose.position.x = goalT[0,-1]
+        mark.pose.position.y = goalT[1,-1]
+        mark.pose.position.z = goalT[2,-1]
+        print(goalT[:3,:3])
+        r = R.from_matrix(goalT[:3,:3]) # rotation matrix
+        (x, y, z, w) = r.as_quat()
+        mark.pose.orientation.x = x
+        mark.pose.orientation.y = y
+        mark.pose.orientation.z = z
+        mark.pose.orientation.w = w
+        mark.color.a = 1
+        mark.color.r = 1
+        mark.color.b = 0
+        mark.color.g = 0
+        mark.scale.x= .1
+        mark.scale.y= .1
+        mark.scale.z= .1
+        mark.lifetime.secs = 0 # FOREVER
+        print(mark)
+        markPub = rospy.Publisher("/visualization_marker", Marker, queue_size=2)
+        rospy.sleep(0.5)
+        markPub.publish(mark)
         
 
         self.tf = TransformListener()
+        
 
         rospy.sleep(0.5)
         
@@ -116,147 +158,189 @@ class holistic_control():
 
     def main_loop(self, goalT):
 
+        spatial_error = 1 # init
+        while spatial_error >= 0.005:
+
+            ### GET world to end effector transformation ###
+            try: 
+                root_joint_name = "panda_link8"
+                position, quaternion = self.tf.lookupTransform("world", root_joint_name, rospy.Time())      
+                r = R.from_quat(quaternion)
+                w2e_T = np.hstack((r.as_matrix(), np.asarray(position).reshape((3,1))))
+                w2e_T = np.vstack((w2e_T, np.array(([0,0,0,1]))))   
+                # print(f"w2e_T {w2e_T}")
 
 
+                # Get current angle of base
+                root_joint_name = "base_footprint"
+                w2b_pose, w2b_orient = self.tf.lookupTransform("world", root_joint_name, rospy.Time()) 
 
-        ### GET world to end effector transformation ###
-        try: 
-            root_joint_name = "panda_link8"
-            position, quaternion = self.tf.lookupTransform("world", root_joint_name, rospy.Time())      
-            r = R.from_quat(quaternion)
-            w2e_T = np.hstack((r.as_matrix(), np.asarray(position).reshape((3,1))))
-            w2e_T = np.vstack((w2e_T, np.array(([0,0,0,1]))))   
-            print(f"w2e_T {w2e_T}")
-        except Exception as e:
-            print(e)
-            return #just retry instantly
+            except Exception as e:
+                print(e)
+                return #just retry instantly
+            
+            # print(f"w2e_T: {w2e_T}")
+            # Grab Manipulator Jacobian in EE frame
+            # mJacob = arm_utilities.mJacobian(self.S, self.arm_dof, self.arm_joint_states)
+            arm_joints = self.arm_joint_states
+            base_joints = self.base_joint_states
+            base_xy = base_joints[:2]
+
+            q_h = np.hstack((np.array([0, 0]), arm_joints))
+            self.panda_base_arm.q = q_h # force it to update
+            # self.panda_arm_base.plot(q_h)
+
+            # Test plot accuracy
+            # print(f"q_h: {q_h}")
+            # self.panda_base_arm.plot(q_h)
+            # rospy.sleep(10000)
+
+            
+            mJacob = self.panda_base_arm.jacobe(q_h)
+            # print(f"mJacob: {mJacob}")
+            # Make Equality Constraints
+            Aeq = np.c_[mJacob, np.eye(6)]
+            
+            fkine = self.panda_base_arm.fkine(q_h)
+            # print(f"fkine.A {fkine.A}")
+            # return
+            v, _ = rtb.p_servo(w2e_T, goalT, 1.5)
+            # v[3:] *= 1.3 # rotate faster?
+            
+            beq = v.reshape((6,))
+
+            # print(f"beq: {beq}")
+            ## The Inequality Constraints for joint limit avoidance ##
+            Ain = np.zeros((self.arm_dof+2 + 6, self.arm_dof+2 + 6))
+            bin = np.zeros(self.arm_dof+2 + 6)
+
+            # The minimum angle (in radians) in which the joint is allowed to approach
+            # to its limit
+            ps = 0.2
+
+            # The influence angle (in radians) in which the velocity damper
+            # becomes active
+            pi = 0.9
+
+            # Form the joint limit velocity damper
+            # print(f"armjoints: {arm_joints}")
+            
+            
+            # print(f"qlim: {self.panda_arm.qlim}")
+            # print(f"q_h: {q_h}")
+            qlim_h = self.panda_base_arm.qlim
+            # print(f"qlim_h {qlim_h}")
+            Ain[: self.arm_dof+2, : self.arm_dof+2], bin[: self.arm_dof+2] = arm_utilities.joint_velocity_damper(ps, pi, n=self.arm_dof+2, q=q_h, qlim=qlim_h)
+            # print(f"Ain: {Ain.shape}\nAin {Ain}")
+            # print(f"bin: {bin.shape}\nBin {bin}")
+            # Form into c
+            # c = np.concatenate(
+            #     (np.zeros(2), mJacob.reshape(self.arm_dof - 2), np.zeros(6))
+            # )
+            
+            # Linear component of objective function: the manipulability Jacobian
         
-        print(f"w2e_T: {w2e_T}")
-        # Grab Manipulator Jacobian in EE frame
-        # mJacob = arm_utilities.mJacobian(self.S, self.arm_dof, self.arm_joint_states)
-        arm_joints = self.arm_joint_states
-        base_joints = self.base_joint_states
-        base_xy = base_joints[:2]
+            # print(f"jacobm: {-self.panda_arm.jacobm(start=self.panda_arm.links[4]).reshape((self.arm_dof))}")
+            # print(f"jacobm before reshape: {-self.panda_arm.jacobm(start=self.panda_arm.links[4])}")
+            c = np.concatenate(
+                (np.zeros(2), -self.panda_arm.jacobm(q=arm_joints).reshape((self.arm_dof)), np.zeros(6))
+            )
+            
+            # Get base to face end-effector
+            kε = 0.5
+            # b2e_T = arm_utilities.fkine(self.S, self.M, self.arm_dof, self.arm_joint_states)
+            b2e_T = self.panda_arm.fkine(q=arm_joints).A
+            # print(f"b2e_t {b2e_T}")
+            θε = np.arctan2(b2e_T[1, -1], b2e_T[0, -1])
+            ε = kε * θε
+            c[0] = -ε
 
-        q_h = np.hstack((np.array([0, 0]), arm_joints))
-        self.panda_base_arm.q = q_h # force it to update
-        # self.panda_arm_base.plot(q_h)
 
-        # Test plot accuracy
-        print(f"q_h: {q_h}")
-        # self.panda_base_arm.plot(q_h)
-        # rospy.sleep(10000)
+            # The lower and upper bounds on the joint velocity and slack variable
+            lb = -np.r_[np.array(([2,0.5])), np.asarray(self.qd_Limits), 10 * np.ones(6)]
+            # print(f"lb: {lb}")
+            ub = np.r_[np.array(([2,0.5])), np.asarray(self.qd_Limits), 10 * np.ones(6)]
 
+            errorT = np.linalg.inv(w2e_T) @ goalT
+            # print(f"eTep: {errorT}")
+            spatial_error = np.sum(np.abs(errorT[:3, -1]))
+            # print(f"Spatial Error: {spatial_error}")
+            Q = np.eye(self.arm_dof + 2 + 6 )
+            k_a = 0.01
+            Q[: self.arm_dof+2, : self.arm_dof+2] *= k_a # apply k_a to arm joints in Q
+            Q[:2, :2] *= 1.0/spatial_error
+
+            # Slack Q
+            Q[self.arm_dof+2 :, self.arm_dof+2 :] = (1.0 / spatial_error) * np.eye(6)
+            
+            # print(f"Q: {Q}")
+            # print(f"c: {c.shape}")
+            # print(f"lb {lb.shape}")
+            # print(f"ub: {ub.shape}")
+            # print(f"Aeq: {Aeq.shape}")
+            # print(f"Beq: {beq.shape}")
+            qd = solve_qp(Q, c, Ain, bin, Aeq, beq, lb=lb, ub=ub, solver='cvxopt')
+            # qd = qd[: self.arm_dof+2]
+            # print(f"qd: {qd}")
+
+
+            # Send motion to arm joints
+            arm_qd = [qd[2], qd[3], qd[4], qd[5], qd[6], qd[7], qd[8]]
+            #self.sendArmVels_trajectories(vels)
+            self.sendArmVels(arm_qd)
+
+            # Send motion to base
+            # mecanum_FK(self, w_fl, w_fr, w_rl, w_rr, r, lx, ly))
+            
+            # Transform Qd into sent joint commands
+            # Turn Virtual Joints into motion
+            qd_bθ = qd[0]
+            qd_bδ = qd[1]
+            w_l = (qd_bδ - self.W * qd_bθ)/self.R
+            w_r = (qd_bθ * self.W)/self.R + w_l
+
+            (Vx_base, Vy_base, W_z_base) = arm_utilities.mecanum_FK(w_l, w_r, w_l, w_r, self.R, self.lx, self.ly)
+
+            euler = euler_from_quaternion(w2b_orient) # translate to euler
+            curr_val = euler[2] # z rotation
+            # print(f"Euler Angles: {curr_val}")
+
+            if curr_val < 0:
+                curr_val = curr_val + 2*np.pi # maps from 0-2pi now
+
+
+            # Create 2D rotation matrix around local chassis Z axis (rotation)
+            R_z = np.array(([np.cos(curr_val), -np.sin(curr_val), 0],
+                            [np.sin(curr_val), np.cos(curr_val), 0],
+                            [0, 0, 1]))
+
+            # Rotate Robot XY velocities to World XY Velocities 
+            worldV_XY = R_z@np.array(([Vx_base, Vy_base, 0]))
+
+            self.Vx_world.publish(worldV_XY[0])
+            self.Vy_world.publish(worldV_XY[1])
+            self.W_z_world.publish(W_z_base)
+
+            print(f"{W_z_base} {qd_bθ} {W_z_base-qd_bθ}")
         
-        mJacob = self.panda_base_arm.jacobe(q_h)
-        print(f"mJacob: {mJacob}")
-        # Make Equality Constraints
-        Aeq = np.c_[mJacob, np.eye(6)]
-        
-        fkine = self.panda_base_arm.fkine(q_h)
-        print(f"fkine.A {fkine.A}")
-        v, _ = rtb.p_servo(fkine.A, goalT, 1.5)
-        v[3:] *= 1.3 # rotate faster?
-        
-        beq = v.reshape((6,))
-
-        print(f"beq: {beq}")
-        ## The Inequality Constraints for joint limit avoidance ##
-        Ain = np.zeros((self.arm_dof+2 + 6, self.arm_dof+2 + 6))
-        bin = np.zeros(self.arm_dof+2 + 6)
-
-        # The minimum angle (in radians) in which the joint is allowed to approach
-        # to its limit
-        ps = 0.2
-
-        # The influence angle (in radians) in which the velocity damper
-        # becomes active
-        pi = 0.9
-
-        # Form the joint limit velocity damper
-        print(f"armjoints: {arm_joints}")
+        self.sendArmVels([0,0,0,0,0,0,0])
+        self.Vx_world.publish(0)
+        self.Vy_world.publish(0)
+        self.W_z_world.publish(0)
         
         
-        # print(f"qlim: {self.panda_arm.qlim}")
-        print(f"q_h: {q_h}")
-        qlim_h = self.panda_base_arm.qlim
-        print(f"qlim_h {qlim_h}")
-        Ain[: self.arm_dof+2, : self.arm_dof+2], bin[: self.arm_dof+2] = arm_utilities.joint_velocity_damper(ps, pi, n=self.arm_dof+2, q=q_h, qlim=qlim_h)
-        print(f"Ain: {Ain.shape}\nAin {Ain}")
-        print(f"bin: {bin.shape}\nBin {bin}")
-        # Form into c
-        # c = np.concatenate(
-        #     (np.zeros(2), mJacob.reshape(self.arm_dof - 2), np.zeros(6))
-        # )
-        
-        # Linear component of objective function: the manipulability Jacobian
-    
-        # print(f"jacobm: {-self.panda_arm.jacobm(start=self.panda_arm.links[4]).reshape((self.arm_dof))}")
-        # print(f"jacobm before reshape: {-self.panda_arm.jacobm(start=self.panda_arm.links[4])}")
-        c = np.concatenate(
-            (np.zeros(2), -self.panda_base_arm.jacobm(start=self.panda_base_arm.links[4]).reshape((self.arm_dof)), np.zeros(6))
-        )
-        
-        # Get base to face end-effector
-        kε = 0.5
-        # b2e_T = arm_utilities.fkine(self.S, self.M, self.arm_dof, self.arm_joint_states)
-        b2e_T = self.panda_arm.fkine(q=arm_joints).A
-        print(f"b2e_t {b2e_T}")
-        θε = np.arctan2(b2e_T[1, -1], b2e_T[0, -1])
-        ε = kε * θε
-        c[0] = -ε
-
-
-        # The lower and upper bounds on the joint velocity and slack variable
-        lb = -np.r_[np.array(([4,4])), np.asarray(self.qd_Limits), 10 * np.ones(6)]
-        # print(f"lb: {lb}")
-        ub = np.r_[np.array(([4,4])), np.asarray(self.qd_Limits), 10 * np.ones(6)]
-
-        errorT = np.linalg.inv(w2e_T) @ goalT
-        print(f"eTep: {errorT}")
-        spatial_error = np.sum(np.abs(errorT[:3, -1]))
-        print(f"Spatial Error: {spatial_error}")
-        Q = np.eye(self.arm_dof + 2 + 6 )
-        k_a = 0.01
-        Q[: self.arm_dof+2, : self.arm_dof+2] *= k_a # apply k_a to arm joints in Q
-        Q[:2, :2] *= 1.0/spatial_error
-
-        # Slack Q
-        Q[self.arm_dof+2 :, self.arm_dof+2 :] = (1.0 / spatial_error) * np.eye(6)
-        
-        print(f"Q: {Q}")
-        print(f"c: {c.shape}")
-        print(f"lb {lb.shape}")
-        print(f"ub: {ub.shape}")
-        print(f"Aeq: {Aeq.shape}")
-        print(f"Beq: {beq.shape}")
-        qd = solve_qp(Q, c, Ain, bin, Aeq, beq, lb=lb, ub=ub, solver='cvxopt')
-        # qd = qd[: self.arm_dof+2]
-        print(f"qd: {qd}")
-
-
-        # Transform Qd into sent joint commands
-        # Turn Virtual Joints into motion
-        # qd_bθ = qd[0]
-        # qd_bδ = qd[1]
-        # w_l = (qd_bδ - self.W * qd_bθ)/self.R
-        # w_r = (qd_bθ * self.W)/self.R + w_l
-
-        ## TODO
-        # send w_r and w_r to simulated_mecanum_logic mecanum_FK(w_fl, w_fr, w_rl, w_rr, r, lx, ly)
-
-        # Send motion to arm joints
-        vels = [qd[2], qd[3], qd[4], qd[5], qd[6], qd[7], qd[8]]
-        #self.sendArmVels_trajectories(vels)
-        self.sendArmVels(vels)
 
 
 
 if __name__ == "__main__":
-    holistic = holistic_control()
-    goalT = np.array(([-1, 0, 0, 5],
-                      [0, 1, 0, 0],
-                      [0, 0, -1, 1.2],
+    
+    goalT = np.array(([-1, 0, 0, 0],
+                      [0, 1, 0, 2.3],
+                      [0, 0, -1, 1.45],
                       [0, 0, 0, 1]))
+
+    holistic = holistic_control(goalT=goalT)
+    # holistic.__init__(goalT)
+
     holistic.main_loop(goalT)
     # rospy.spin()